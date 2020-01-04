ef7553572bad58ef34b4d32110faf1f5c8c96b82
Luis Flavio <luis.pereirasiu@gmail.com>
2020-01-04 14:06:16 -0300

    Conclusao das configurações iniciais para o o inicio do projeto

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->addErrorMiddleware(false, true, true);
+$app->addErrorMiddleware(true, true, true);
 
-require __DIR__ . '/routes.php';
+require __DIR__.'/routes.php';
 
 $app->run();
--- routes.php
+++ routes.php
@@ -3,7 +3,10 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
+$app->setBasePath(URL_BASE);
+
 $app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write("Hello");
+    $response->getBody()->write('Hello');
+
     return $response;
 });
